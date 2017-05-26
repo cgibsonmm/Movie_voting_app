9e106cca8b4adda10e340fd2d9173105e4f1f401
cgibsonmm <cgibsonmm@gmail.com>
2017-05-26 01:18:26 -0400

    small styling enhancments, and movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ApplicationRecord
 	acts_as_votable
 	has_attached_file :image, styles: { medium: "350x350>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :title, length: { in: 2..50 }
+  validates :image, presence: true
 end
